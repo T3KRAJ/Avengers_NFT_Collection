33f4402cef623bec1987dcccc1a3d12f3ab722fe
Tek Raj Joshi <tekrajjoshi07@gmail.com>
2022-08-31 11:30:33 +0530

    avengers png added

--- next_app/pages/api/[tokenId].js
+++ next_app/pages/api/[tokenId].js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   const tokenId = req.query.tokenId;
   const image_url =
-    "https://github.com/T3KRAJ/Avengers_NFT_Collection/tree/main/next_app/public/assets";
+    "https://raw.githubusercontent.com/T3KRAJ/Avengers_NFT_Collection/main/next_app/public/assets/";
   res.status(200).json({
     name: "Avengers NFT #" + tokenId,
     description: "Avengers NFT is a collection of avengers in crypto",
